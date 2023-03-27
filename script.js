88238f96c43120b212e3840bd3e15a942f1a45fb
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-27 11:33:53 -0300

    testelet

---
+++ script.js
@@ -0,0 +1,46 @@
+//alert simples
+
+//alert("Olá Mundo")
+
+//alert de confirmação
+//let msg = confirm("Você é um aluno?")
+
+//alert passando valores em campos
+
+//let msg1 = prompt("Digite seu nome:")
+//document.write("Meu nome é: ", msg1, "!")
+
+//condição if
+//if(10 > 7){
+    //document.write("Dez é maior que sete!")
+    //document.write("<br>")
+
+//}
+//let valor = 20
+//if ( valor < 10){
+
+    //document.write( +valor+ " é maior que sete!")
+
+//}else{
+    //document.write("O valor é menor")
+//}
+
+/*
+let idade = prompt("Digite sua Idade: ")
+if (idade <=12){
+    document.write("É uma criança")
+} else if(idade <= 17){
+    document.write("É um adolescente")
+} else if(idade <=29){
+    document.write("É um jovem")
+} else{
+    document.write("É um adulto")
+}
+*/
+
+let valor = prompt("Digite um valor: ")
+if (valor > 10){
+    document.write("O valor é Maior que 10")
+} else{
+    document.write("O valor é Menor que 10")
+}
